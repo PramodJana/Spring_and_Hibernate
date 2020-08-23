259e4149a9f8660d16e9183fde67458086946ff5
Pramod Jana <32437635+PramodJana@users.noreply.github.com>
2020-08-23 14:37:04 +0530

    created controllers and view

---
+++ spring-mvc-demo/src/com/luv2code/springdemo/mvc/HelloWorldController.java
@@ -0,0 +1,45 @@
+package com.luv2code.springdemo.mvc;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller 
+public class HelloWorldController {
+
+	// need a controller method to show the initial html form
+	
+	@RequestMapping("/showForm")
+	public String showForm() {
+		return "helloworld-form";
+	}
+	
+	
+	
+	//need a controller method to process the html form
+	
+	@RequestMapping("/processForm")
+	public String processForm() {
+		return "helloworld";
+	}
+	
+	
+	//new controller method to read form data and
+	// add data to the model
+	
+	@RequestMapping("/processFormVersionTwo")
+	public String letsShoutDude(HttpServletRequest request, Model model) {
+		String theName = request.getParameter("studentName");
+		
+		theName = theName.toUpperCase();
+		
+		String result = "Yo! " + theName;
+		
+		model.addAttribute("message", result);
+		
+		return "helloworld";
+	}
+	
+}
