1095546d92f66ba32d2208b411181ff1d0986b38
Pramod Jana <32437635+PramodJana@users.noreply.github.com>
2020-10-31 17:59:30 +0530

    11

---
+++ test/src/test/Employee.java
@@ -0,0 +1,16 @@
+package test;
+
+public class Employee {
+//int employeeid;
+//String name;
+//public String Employee(int employeeid, String name)
+//{
+//	this.employeeid=employeeid;
+//	this.name=name;
+//}
+void Employee()
+{
+	System.out.println("welcome to our family");
+}
+
+}
---
+++ test/src/test/HR.java
@@ -0,0 +1,12 @@
+package test;
+
+public class HR {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Employee empobj= new Employee();
+		System.out.println("have a good day");
+
+	}
+
+}
